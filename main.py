c746bb3a8c60b06000eb387f44e72e198eb750b6
lukesp2005 <lukesp2005@gmail.com>
2024-12-17 14:49:41 +0000

    avec exo truc

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
+
+from math import log, sqrt
+
 def factorielle(n):
   p=1
   for k in range (1,n+1):
     p=p*k
   return p
 
-def sommefacto(n):
+def sommefacto(k):
   somme=0
-  for k in range  (0,n+1):
+  for k in range  (0,k+1):
     somme=somme+1/factorielle(k)
   return somme
 
-print(sommefacto(100))
-  
+
+def ex111 (x): return 1/(log(3*x)+1)
+def ex112 (x): return 
+ex112 = lambda x : x/(sqrt(2*x-1))
+def sommeg(debut,fin,fonction):
+  somme = 0 
+  for i in range(debut,fin+1):
+    somme += fonction(i)
+  return somme
+
+
+
+
+
+
+
+def facto(n):
+  if n==0:
+    return 1
+  else:
+    return n*facto(n-1) 
+
+def combi(n,p):
+  somme=facto(n)/(facto(p)*facto(n-p))
+  return somme
+
+
+def syra(x,c):
+  for c in range(0,c+1):
+    if x%2 == 0 :
+      x=x/2
+    else:
+      x=x*3+1
+    print(x)
+
+print(syra(6,12))
